ed09a1a189bf7ecf6c7eadeb17f2261317ff1787
payal2604 <91472774+payal2604@users.noreply.github.com>
2023-11-21 13:55:33 +0530

    Update Uberprice(ML1).py

--- Uberprice(ML1).py
+++ Uberprice(ML1).py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +12,7 @@ import warnings
 #We do not want to see warnings
 warnings.filterwarnings("ignore")
 
-data = pd.read_csv("Desktop/uber.csv")
+data = pd.read_csv("D:\Downloads\Material\BE COMP\SEM 7\LP 3\ML\predict the priceof uber rides ml1/uber.csv")
 
 df = data.copy()
 
@@ -27,18 +28,71 @@ df.describe()
 
 df.isnull().sum()
 
-df.corr()
+corr=df.corr()
+corr
+corr = df.corr()
+corr.style.background_gradient(cmap='BuGn')
 
 df.dropna(inplace=True)
 
 plt.boxplot(df['fare_amount'])
+plt.show()
 
 q_low = df["fare_amount"].quantile(0.01)
 q_hi  = df["fare_amount"].quantile(0.99)
 
 df = df[(df["fare_amount"] < q_hi) & (df["fare_amount"] > q_low)]
-
 df.isnull().sum()
+
+
+
+
+
+incorrect_coordinates = df.loc[
+    (df.pickup_latitude > 90) |(df.pickup_latitude < -90) |
+    (df.dropoff_latitude > 90) |(df.dropoff_latitude < -90) |
+    (df.pickup_longitude > 180) |(df.pickup_longitude < -180) |
+    (df.dropoff_longitude > 90) |(df.dropoff_longitude < -90)
+]
+df.drop(incorrect_coordinates, inplace = True, errors = 'ignore')
+def distance_transform(longitude1, latitude1, longitude2, latitude2):
+    long1, lati1, long2, lati2 = map(np.radians, [longitude1, latitude1, longitude2, latitude2])
+    dist_long = long2 - long1
+    dist_lati = lati2 - lati1
+    a = np.sin(dist_lati/2)**2 + np.cos(lati1) * np.cos(lati2) * np.sin(dist_long/2)**2
+    c = 2 * np.arcsin(np.sqrt(a)) * 6371
+    # long1,lati1,long2,lati2 = longitude1[pos],latitude1[pos],longitude2[pos],latitude2[pos]
+    # c = sqrt((long2 - long1) ** 2 + (lati2 - lati1) ** 2)asin
+
+    return c
+df['Distance'] = distance_transform(
+    df['pickup_longitude'],
+    df['pickup_latitude'],
+    df['dropoff_longitude'],
+    df['dropoff_latitude']
+)
+
+
+
+
+
+plt.scatter(df['Distance'], df['fare_amount'])
+plt.xlabel("Distance")
+plt.ylabel("fare_amount")
+plt.show()
+
+
+df.drop(df[df['Distance'] >= 60].index, inplace = True)
+df.drop(df[df['fare_amount'] <= 0].index, inplace = True)
+
+df.drop(df[(df['fare_amount']>100) & (df['Distance']<1)].index, inplace = True )
+df.drop(df[(df['fare_amount']<100) & (df['Distance']>100)].index, inplace = True )
+
+plt.scatter(df['Distance'], df['fare_amount'])
+plt.xlabel("Distance")
+plt.ylabel("fare_amount")
+plt.show()
+
 
 from sklearn.model_selection import train_test_split
 
@@ -60,7 +114,8 @@ predict = lrmodel.predict(x_test)
 
 from sklearn.metrics import mean_squared_error
 lrmodelrmse = np.sqrt(mean_squared_error(predict, y_test))
-print("RMSE error for the model is ", lrmodelrmse)
+print("RMSE error for the linear Regression model is ", lrmodelrmse)
+print("Predicted price by linear regression", predict)
 
 from sklearn.ensemble import RandomForestRegressor
 rfrmodel = RandomForestRegressor(n_estimators = 100, random_state = 101)
@@ -70,7 +125,7 @@ rfrmodel_pred = rfrmodel.predict(x_test)
 
 rfrmodel_rmse = np.sqrt(mean_squared_error(rfrmodel_pred, y_test))
 print("RMSE value for Random Forest is:",rfrmodel_rmse)
-
+print("Predicted price by Random forest", rfrmodel_pred)
 
 # In[ ]:
 
